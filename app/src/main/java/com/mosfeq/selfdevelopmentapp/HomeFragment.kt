77693687a460b4bf4d526aad5b9d2b1b106cbecb
Mosfeq <mosfeq5000@gmail.com>
2022-03-07 22:56:24 +0000

    8th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.android.synthetic.main.home_fragment.*
+import kotlinx.android.synthetic.main.login_create_account_fragment.*
 
-class LoginFragment : Fragment(R.layout.login_fragment) {
+class HomeFragment : Fragment(R.layout.home_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btn_ConfirmLogin.setOnClickListener{
-            val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+        btn_LoginDone.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginCreateAccountFragment()
             findNavController().navigate(action)
         }
+
     }
+
 }
