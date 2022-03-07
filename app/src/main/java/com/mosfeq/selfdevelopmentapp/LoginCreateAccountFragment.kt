c815ff73d0bccf4de86e01dd013c6e714234417c
Mosfeq <mosfeq5000@gmail.com>
2022-03-07 22:32:44 +0000

    7th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/LoginCreateAccountFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/LoginCreateAccountFragment.kt
@@ -1,6 +1,20 @@
 package com.mosfeq.selfdevelopmentapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.login_create_account_fragment.*
 
 class LoginCreateAccountFragment : Fragment(R.layout.login_create_account_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_login.setOnClickListener{
+            val action = LoginCreateAccountFragmentDirections.actionLoginCreateAccountFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
+
 }
