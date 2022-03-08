da96abaeed23c3c319a4e5e437e2dcf5cc9e900f
Mosfeq <mosfeq5000@gmail.com>
2022-03-08 13:05:01 +0000

    10th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/MainActivity.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.mosfeq.selfdevelopmentapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        bottom_navigator.setupWithNavController(navController)
     }
 }
