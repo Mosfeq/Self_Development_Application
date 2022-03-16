7102428f656ec73c922aaaf6de6900aafaad76da
Mosfeq <mosfeq5000@gmail.com>
2022-03-16 20:07:52 +0000

    13th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
@@ -10,22 +10,50 @@ import kotlinx.android.synthetic.main.login_create_account_fragment.*
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
+    private val habitsList = listGenerator(20)
+    private val adapter = HabitAdapter(habitsList)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recycler_view.adapter = HabitAdapter(listGenerator(20))
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(context)
         recycler_view.setHasFixedSize(true)
+
+        btn_addHabit.setOnClickListener {
+            addHabit(view)
+        }
+
+        btn_deleteHabit.setOnClickListener {
+            deleteHabit(view)
+        }
     }
 
-    private fun listGenerator(sizeOfList: Int): List<HabitItem>{
-        val habitsList = ArrayList<HabitItem>()
+    private fun addHabit(view: View){
+        val index: Int = 0
+
+        val newHabit = HabitItem(
+            "Habit Name",
+            "goal: $index",
+            "reason:"
+        )
+        habitsList.add(index, newHabit)
+        adapter.notifyItemInserted(index)
+    }
+
+    private fun deleteHabit(view: View){
+        val index: Int = 0
+        habitsList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+    private fun listGenerator(sizeOfList: Int): ArrayList<HabitItem>{
+        val list = ArrayList<HabitItem>()
 
         for (i in 0 until sizeOfList){
             val habitItem = HabitItem("Habit Name $i", "Last Date: ", "Reason: ")
-            habitsList+= habitItem
+            list+= habitItem
         }
-        return habitsList
+        return list
     }
-
 }
