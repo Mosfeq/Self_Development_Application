f3372394b4e6df993aaa1766d30a40ee9388565f
Mosfeq <mosfeq5000@gmail.com>
2022-03-18 22:11:33 +0000

    21th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/AddHabitsFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/AddHabitsFragment.kt
@@ -1,31 +1,43 @@
 package com.mosfeq.selfdevelopmentapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.mosfeq.selfdevelopmentapp.databinding.ActivityMainBinding
+import com.mosfeq.selfdevelopmentapp.databinding.AddHabitsFragmentBinding
 import kotlinx.android.synthetic.main.add_habits_fragment.*
 
 class AddHabitsFragment : Fragment(R.layout.add_habits_fragment) {
 
+    private lateinit var binding: AddHabitsFragmentBinding
     private lateinit var database: DatabaseReference
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = AddHabitsFragmentBinding.inflate(layoutInflater)
 
-        val habitName = et_addEnterHabit.text.toString().trim()
-        val goal = et_addEnterGoal.text.toString().trim()
-        val reason = et_addEnterReason.text.toString().trim()
+        val inputtedHabit = binding.etEnterHabit.text.toString()
+        val inputtedGoal = binding.etEnterGoal.text.toString()
+        val inputtedReason = binding.etEnterReason.text.toString()
+
 
         database = FirebaseDatabase.getInstance("https://self-improvement-application-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Habits")
-        val habitItem = HabitItem(habitName, goal, reason)
+        val habitItem = HabitItem(inputtedHabit, inputtedGoal, inputtedReason)
 
-        database.child(habitName).setValue(habitItem).addOnCompleteListener {
-            Toast.makeText(context, "Habit Added", Toast.LENGTH_SHORT).show()
-        }.addOnFailureListener {
-            Toast.makeText(context, "Habit Not Added", Toast.LENGTH_SHORT).show()
+        btn_addTest.setOnClickListener {
+            Log.e("N", "$inputtedHabit, $inputtedGoal, $inputtedReason")
+        }
+
+        btn_addDone.setOnClickListener {
+            database.child(inputtedHabit).setValue(habitItem).addOnCompleteListener {
+                Toast.makeText(context, "Habit Added", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(context, "Habit Not Added", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
