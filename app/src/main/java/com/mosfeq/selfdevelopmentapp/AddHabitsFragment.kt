218d7cfc8c815523784130b9815a682cd191367e
Mosfeq <mosfeq5000@gmail.com>
2022-03-18 20:55:38 +0000

    20th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/AddHabitsFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/AddHabitsFragment.kt
@@ -4,25 +4,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.add_habits_fragment.*
-import kotlinx.android.synthetic.main.home_fragment.*
-import java.util.*
 
-class AddHabitsFragment: Fragment(R.layout.add_habits_fragment) {
+class AddHabitsFragment : Fragment(R.layout.add_habits_fragment) {
+
+    private lateinit var database: DatabaseReference
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val enterHabitName = et_addEnterHabit.text.toString()
-        val enterLastFirstDay = et_addEnterLastFirstDay.text.toString()
-        val enterGoal = et_addEnterGoal.text.toString()
-        val enterReason = et_addEnterReason.text.toString()
+        val habitName = et_addEnterHabit.text.toString().trim()
+        val goal = et_addEnterGoal.text.toString().trim()
+        val reason = et_addEnterReason.text.toString().trim()
 
-        btn_addDone.setOnClickListener{
-            val action = AddHabitsFragmentDirections.actionAddHabitsFragmentToHomeFragment(
-                enterHabitName, enterLastFirstDay, enterReason)
-            findNavController().navigate(action)
+        database = FirebaseDatabase.getInstance("https://self-improvement-application-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Habits")
+        val habitItem = HabitItem(habitName, goal, reason)
+
+        database.child(habitName).setValue(habitItem).addOnCompleteListener {
+            Toast.makeText(context, "Habit Added", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(context, "Habit Not Added", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/HomeFragment.kt
@@ -1,17 +1,14 @@
 package com.mosfeq.selfdevelopmentapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
-    private val args: HomeFragmentArgs by navArgs()
     private val habitsList = listGenerator(20)
     private val adapter = HabitAdapter(habitsList)
 
@@ -23,24 +20,12 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         recycler_view.setHasFixedSize(true)
 
         btn_addHabit.setOnClickListener {
-            addHabitPage(view)
+            val action = HomeFragmentDirections.actionHomeFragmentToAddHabitsFragment()
+            findNavController().navigate(action)
         }
 
         btn_deleteHabit.setOnClickListener {
             deleteHabit(view)
-        }
-
-        btn_Refresh.setOnClickListener{
-            val index: Int = 0
-            val habitName = args.habitName
-            val lastFirstDate = args.lastFirstDay
-            val reason = args.reason
-            Log.d("ARGS","$habitName, $lastFirstDate, $reason")
-
-            val newHabit = HabitItem(habitName, "Last day: $lastFirstDate", "Reason: $reason")
-
-            habitsList.add(index, newHabit)
-            adapter.notifyItemInserted(index)
         }
     }
 
@@ -49,10 +34,10 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 //        findNavController().navigate(action)
 //    }
 
-    private fun addHabitPage(view: View) {
-        val action = HomeFragmentDirections.actionHomeFragmentToAddHabitsFragment()
-        findNavController().navigate(action)
-    }
+//    private fun addHabitPage(view: View) {
+////        val action = HomeFragmentDirections.actionHomeFragmentToAddHabitsFragment()
+////        findNavController().navigate(action)
+//    }
 
 //    private fun addHabit(){
 //        val index: Int = 0
