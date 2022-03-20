b639c81cd297961978d1237d36f4367fe5c61f6b
Mosfeq <mosfeq5000@gmail.com>
2022-03-20 00:44:33 +0000

    24th commit

--- app/src/main/java/com/mosfeq/selfdevelopmentapp/QuotesFragment.kt
+++ app/src/main/java/com/mosfeq/selfdevelopmentapp/QuotesFragment.kt
@@ -2,5 +2,5 @@ package com.mosfeq.selfdevelopmentapp
 
 import androidx.fragment.app.Fragment
 
-class SettingsFragment: Fragment(R.layout.settings_fragment) {
+class QuotesFragment: Fragment(R.layout.quotes_fragment) {
 }
